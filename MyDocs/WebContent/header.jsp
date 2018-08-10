13e93c90dafb77349f011081f479f008cad085f5
Mala Reddy <42225621+reddy3482@users.noreply.github.com>
2018-08-10 08:02:28 +0530

    Add files via upload

---
+++ MyDocs/WebContent/header.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h5>Welcome to mydocs portal &#8208 A safe way of sharing documents</h5>
+</body>
+</html>
---
+++ MyDocs/src/com/controller/loginController.java
@@ -0,0 +1,56 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class loginController
+ */
+@WebServlet("/login")
+public class loginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public loginController() {
+		super();
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if ((request.getParameter("username").equals("Admin")) && (request.getParameter("password").equals("Admin"))) {
+			session.setAttribute("username", username);
+			response.sendRedirect("sucess.jsp");
+		} else {
+			response.sendRedirect("error.jsp");
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
